20a6649862b83f8fc0ec57a5ea5cc8505a1a8daa
Victor <victor@github.com>
2019-10-20 03:47:48 +0530

    Finding by ID added

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./DB/mongoose');
 var {Todo} = require('./Models/todo');
@@ -26,6 +27,23 @@ app.get('/todos', (req,res) => {
     }, (e) => {
         res.ststus(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req,res) => { //:id creates a variable id in the URL for Dynamic id acceptance
+    
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){ //Check Mongoose Queries
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(3000, () => {
